b7d7331920542fd3d0df7c32a0408810cc88d5c9
Co Nguyen <nguye2co@mail.uc.edu>
2025-03-27 11:18:47 -0400

    CN

--- ProjectToShareWithTheClass/Avocado.cs
+++ ProjectToShareWithTheClass/Avocado.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Co Nguyen
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,9 @@ namespace ProjectToShareWithTheClass
 
         // Create getter and a setter for the private properties
         public double Price { get { return _price; } set { _price = value; } }
+
+        //add the getter and setter for the color property
+        public String Color { get { return _color; } set { _color = value; } }
 
     }
 }
