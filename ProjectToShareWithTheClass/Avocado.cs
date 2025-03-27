c4568fe765d561e2afedd19917d84af97c929073
Gabe Galiatsatos <galiatgg@mail.uc.edu>
2025-03-27 11:11:59 -0400

    Added Color property getter in setter

--- ProjectToShareWithTheClass/Avocado.cs
+++ ProjectToShareWithTheClass/Avocado.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,8 +15,9 @@ namespace ProjectToShareWithTheClass
         private String _color;
         private double _price;
 
-        // Create getter and a setter for the private properties
+        //// Create getter and a setter for the private properties
         public double Price { get { return _price; } set { _price = value; } }
-
+        //// Add the getter and setter for the color property
+        public string Color { get { return _color; } set { _color = value; } }
     }
 }
